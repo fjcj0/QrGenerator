bf35dbfb7d324b484bc48aa8dbbac211c4fbb37b
omar <omarqais225@gmail.com>
2025-08-13 21:20:48 +0300

    LINK MONGODB

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 export const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGODB_URI);
+        const conn = await mongoose.connect(process.env.MONGODB_LINK);
         console.log(`MongoDB Connected successfully!!`);
     } catch (error) {
         console.log("MongoDB connection error: ", error.message);
