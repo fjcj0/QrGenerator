224467771d725d7ee2c8f32e5979b8c8f31b8a23
omar <omarqais225@gmail.com>
2025-09-03 02:39:03 +0300

    fix an error

--- backend/src/index.js
+++ backend/src/index.js
@@ -21,7 +21,7 @@ app.use('/api/qr', qrRoutes.default);
 if (process.env.NODE_ENV === "production") {
     const frontendPath = path.resolve(__dirname, "../../frontend/dist");
     app.use(express.static(frontendPath));
-    app.get("/*", (req, res) => {
+    app.get("*", (req, res) => {
         res.sendFile(path.join(frontendPath, "index.html"));
     });
 }
